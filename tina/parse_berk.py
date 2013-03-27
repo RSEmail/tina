0e5a3f8e9b2adfb5c6e34a9ef1ce0ac92594241e
Mat Grove <mat.grove@rackspace.com>
2013-03-27 17:32:14 -0400

    Use berks to try and get all cookbooks.

--- tina/parse_berk.py
+++ tina/parse_berk.py
@@ -1,36 +1,47 @@
 import re
 from urlparse import urlparse
+from subprocess import check_output
+import json
 
-def get_git_urls_from_file(filename):
-    urls = get_urls_from_file(filename)
-    return normalize_urls_to_git(urls)
+def run_berks():
+    data = check_output(["berks", "install", "--path", ".tina/berks", "--format=json"])
+    cookbooks = json.loads(data)
+    for cookbook in cookbooks["cookbooks"]:
+        rw_url = None
+        taggable = False
+        if "location" in cookbook.keys():
+            taggable = False
+            url = get_url_from_string(cookbook["location"])
+            rw_url = normalize_urls_to_git(url)
+            if rw_url:
+                regex = re.compile(r'.*rackspace.*')
+                match = regex.match(rw_url)
+                if match:
+                    taggable = True
+                
+        cookbook["rw_url"] = rw_url
+        cookbook["taggable"] = taggable
+
+    return cookbooks["cookbooks"]
+
 
 #TODO: always fails if urls are already git@github.com:repo.git
-def normalize_urls_to_git(urls):
-    normalized_urls = []
-    for url in urls:
-        parts = urlparse(url)
-        if not parts.hostname  or not parts.path:
-            raise SyntaxError("URL is malformed: '%s'" % url)
-        norm = "git@" + parts.hostname
-        norm += ":" + parts.path.lstrip("/")
-        if not norm.endswith(".git"):
-            norm += ".git"
-        normalized_urls.append(norm)
-    return normalized_urls
+def normalize_urls_to_git(url):
+    if url == None:
+        return None
+    parts = urlparse(url)
+    if not parts.hostname or not parts.path:
+        raise SyntaxError("URL is malformed: '%s'" % url)
+    norm = "git@" + parts.hostname
+    norm += ":" + parts.path.lstrip("/")
+    if not norm.endswith(".git"):
+        norm += ".git"
+    return norm
 
-def get_urls_from_file(filename):
-    urls = []
-    try:
-        raw = open(filename, "r")
-        regex = re.compile(r'[\'\"][a-zA-Z]*://.*?[\'\"]')
-        for line in raw:
-            matches = regex.findall(line)
-            for word in matches:
-                urls.append(word.replace('\"',''))
-    except IOError as e:
-        print "Unable to open file to parse it '{0}': '{1}'".format(filename, e.strerror)
-        raise
-    else:
-        raw.close()
-    return urls
+def get_url_from_string(line):
+    regex = re.compile(r'.*[\'\"]([a-zA-Z]*://.*?)[\'\"]')
+    match = regex.match(line)
+    if not match:
+        return None
+    word = match.group(1)
+    return (word.replace('\"',''))
