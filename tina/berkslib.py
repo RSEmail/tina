395489df836394ef88fe443eb6c693917998bc1a
Alex Corvin <acorvin@fedora-desktop.alexcorvin.com>
2015-03-20 17:05:01 -0400

    Update tina to work with new berkshelf
    * Berks install was replaced in the new version by berks vendor
    
    * Berks vendor no longer displays the locations of our cookbooks that
    are in git. The new berks list functionaly was added to determine the
    git repository urls

--- tina/berkslib.py
+++ tina/berkslib.py
@@ -13,21 +13,40 @@ def get_name_from_url(repo_url):
 
 def repos_from_berks():
     data = _get_json_from_berks_run()
+    locations = _get_locations_from_berks_list()
     repos = {}
     for repo in data["cookbooks"]:
         url = None
         if "location" in repo.keys():
             url = _get_url_from_string(repo["location"])
+        elif locations.has_key(repo['name']):
+            url = locations[repo['name']]
         # name is the local_dir the repo was checked out into
         repos[repo["name"]] = url
     return repos
 
 def _get_json_from_berks_run():
     print "Running Berkshelf..."
-    proc = subprocess.Popen(["berks", "install", "--path", ".tina", 
+    proc = subprocess.Popen(["berks", "vendor", ".tina", 
         "--format=json"], stdout=subprocess.PIPE)
     berks_output, errors = proc.communicate()
     return json.loads(berks_output)
+
+def _get_locations_from_berks_list():
+    locations = {}
+    proc = subprocess.Popen(["berks", "list"], stdout=subprocess.PIPE)
+    for line in proc.stdout:
+        line = line.strip()
+        if not line.startswith('*') or not 'from' in line or\
+            'github' not in line:
+            continue
+        line_parts = line.split('from')
+        cookbook_part = line_parts[0]
+        cookbook = cookbook_part.split(' ')[1]
+        source_part = line_parts[1].strip()
+        url = source_part.split(' ')[0].strip()
+        locations[cookbook] = url
+    return locations
 
 def _get_url_from_string(line):
     regex = re.compile(".*['\"]((https://|git://|git@).*?)['\"]")
